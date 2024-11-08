5a69b83e7f7e8d210d33b1fbe544fd3b4a939fa2
zinab27 <68290835+zinab27@users.noreply.github.com>
2024-11-08 22:35:48 +0200

    output file and time

--- CarRunnable.java
+++ CarRunnable.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CarRunnable implements Runnable {
 
     private final ParkingLot parkingLot;
@@ -14,31 +17,70 @@ public class CarRunnable implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(car.getCarArrival() * 100);
-            System.out.println("Car " + car.getCarNumber() + " arrived at time " + car.getCarArrival() + " from Gate "
-                    + gate.getGateNumber());
 
+            Thread.sleep(car.getCarArrival() * 100);
+            try (FileWriter writer = new FileWriter("Output.txt")) {
+                writer.write("Car " + car.getCarNumber() + " from Gate " + gate.getGateNumber() + " arrived at time "
+                        + car.getCarArrival() + "\n");
+                writer.flush();
+            } catch (IOException e) {
+
+            }
+
+            long startWaitTime = 0;
             synchronized (parkingLot) {
                 while (!parkingLot.tryParkCar()) {
                     if (!hasPrintedWaitingMessage) {
-                        System.out.println("Car " + car.getCarNumber() + " waiting for a spot.");
+                        try (FileWriter writer = new FileWriter("Output.txt", true)) {
+                            writer.write("Car " + car.getCarNumber() + " from Gate " + gate.getGateNumber()
+                                    + " waiting for a spot.\n");
+                            writer.flush();
+                        } catch (IOException e) {
+
+                        }
                         hasPrintedWaitingMessage = true;
                     }
+                    startWaitTime = System.currentTimeMillis();
+                    // car.incrementWaitingTime();
                     parkingLot.wait();
                 }
-                double waited = (System.currentTimeMillis() - car.getCarArrival() * 100) / 1000.0;
-                System.out.println("Car " + car.getCarNumber() + " parked," + " from Gate " + gate.getGateNumber()
-                        + " . Waited: " + waited + "s. (Current Spots: "
-                        + parkingLot.getCurrentCars() + ")");
+
+                if (hasPrintedWaitingMessage) {
+                    long waitTime = System.currentTimeMillis() - startWaitTime;
+
+                    try (FileWriter writer = new FileWriter("Output.txt", true)) {
+                        writer.write("Car " + car.getCarNumber() + " from Gate " + gate.getGateNumber() + " parked"
+                                + " after waiting for " + (waitTime / 100 == 0 ? 1 : waitTime / 100) + " units of time."
+                                + " (Parking Status: " + parkingLot.getCurrentCars() + " spots occupied)\n");
+                        writer.flush();
+                    } catch (IOException e) {
+
+                    }
+                } else {
+
+                    try (FileWriter writer = new FileWriter("Output.txt", true)) {
+                        writer.write("Car " + car.getCarNumber() + " from Gate " + gate.getGateNumber() + " parked."
+                                + " (Parking Status: " + parkingLot.getCurrentCars()
+                                + " spots occupied)\n");
+                        writer.flush();
+                    } catch (IOException e) {
+
+                    }
+                }
             }
-
             Thread.sleep(car.getCarDuration() * 100);
-
             synchronized (parkingLot) {
                 parkingLot.leaveSpot();
-                System.out.println("Car " + car.getCarNumber() + " left. " + " from Gate " + gate.getGateNumber()
-                        + " (Current Spots: " + parkingLot.getCurrentCars() + ")");
-                parkingLot.notify();
+
+                try (FileWriter writer = new FileWriter("Output.txt", true)) {
+                    writer.write("Car " + car.getCarNumber() + " from Gate " + gate.getGateNumber() + " left after "
+                            + car.getCarDuration()
+                            + " units of time. (Parking Status: " + parkingLot.getCurrentCars() + " spots occupied)\n");
+                    writer.flush();
+                } catch (IOException e) {
+
+                }
+                parkingLot.notify(); // Notify other threads waiting for a spot
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt(); // Properly handle interruption
